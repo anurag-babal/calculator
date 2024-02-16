fed1408f767e5ae12053b8851c2a4842d569ea44
anurag-babal <babalanurag2@gmail.com>
2024-02-16 23:59:21 +0530

    Add packaging and main class configuration to pom.xml, and implement user input in Calculator.java

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -1,6 +1,20 @@
 package com.example.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    Scanner scanner = new Scanner(System.in);
+    int first, second;
+    System.out.print("Enter first number: ");
+    first = scanner.nextInt();
+    System.out.print("Enter second number: ");
+    second = scanner.nextInt();
+    System.out.println("Result is : " + calculator.add(first, second));
+    scanner.close();
+  }
 
   public double add(double num1, double num2) {
       return num1 + num2;
