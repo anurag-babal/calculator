c069ff7ba299ad7569269e0ae51f1b9619e4a113
anurag-babal <babalanurag2@gmail.com>
2024-02-21 22:34:18 +0530

    Add exit option to menu

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -15,6 +15,7 @@ public class Calculator {
         System.out.println("2. Subtraction");
         System.out.println("3. Multiplication");
         System.out.println("4. Division");
+        System.out.println("0. Exit");
         System.out.print("Enter your choice:");
         choice = scanner.nextInt();
         System.out.print("Enter first number: ");
