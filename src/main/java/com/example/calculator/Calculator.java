6b86288e1c79bc29933a2eee413b0d790105cabd
anurag-babal <babalanurag2@gmail.com>
2024-02-29 23:51:14 +0530

    Refactor calculator class to use double instead of int for input values

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -8,8 +8,9 @@ public class Calculator {
 		Calculator calculator = new Calculator();
 		Scanner scanner = new Scanner(System.in);
 		int choice;
-		int first, second;
-		while (true) {
+		double first, second;
+		boolean status = true;
+		while (status) {
 			System.out.println("1. Square Root");
 			System.out.println("2. Factorial");
 			System.out.println("3. Natural logarithm");
@@ -17,23 +18,29 @@ public class Calculator {
 			System.out.println("0. Exit");
 			System.out.print("Enter your choice:");
 			choice = scanner.nextInt();
-			if (choice == 0) {
-				break;
-			}
-			System.out.print("Enter number: ");
-			first = scanner.nextInt();
 			switch (choice) {
+				case 0:
+					status = false;
+					break;
 				case 1:
+					System.out.print("Enter number: ");
+					first = scanner.nextInt();
 					System.out.println("Result is : " + calculator.root(first));
 					break;
 				case 2:
+					System.out.print("Enter number: ");
+					first = scanner.nextInt();
 					System.out.println("Result is : " + calculator.factorial(first));
 					break;
 				case 3:
 					System.out.println("Not Yet Implemented");
-					// System.out.println("Result is : " + calculator.log(first));
+					System.out.print("Enter number: ");
+					first = scanner.nextDouble();
+					System.out.println("Result is : " + calculator.log(first));
 					break;
 				case 4:
+					System.out.print("Enter number: ");
+					first = scanner.nextInt();
 					System.out.print("Enter exponent: ");
 					second = scanner.nextInt();
 					System.out.println("Result is : " + calculator.power(first, second));
@@ -45,25 +52,25 @@ public class Calculator {
 		scanner.close();
 	}
 
-	public double root(int num1) {
+	public double root(double num1) {
 		return Math.sqrt(num1);
 	}
 
-	public int factorial(int num1) {
+	public int factorial(double num1) {
 		int ans = 1;
 		for (int i = 1; i <= num1; i++)
 			ans = ans * i;
 		return ans;
 	}
 
-	public double log(int num1) {
+	public double log(double num1) {
 		double ans;
 		ans = Math.log(num1);
 		ans = Math.round(ans * Math.pow(10, 2)) / Math.pow(10, 2);
 		return ans;
 	}
 
-	public double power(double num1, double num2) {
-		return Math.pow(num1, num2);
+	public int power(double num1, double num2) {
+		return (int) Math.pow(num1, num2);
 	}
 }
