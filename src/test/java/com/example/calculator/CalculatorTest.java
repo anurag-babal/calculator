9b899b3de92c99d8e50b0c0b64d7e3ae9ac7f232
anurag-babal <babalanurag2@gmail.com>
2024-03-01 00:18:02 +0530

    Fix data types in CalculatorTest.java

--- src/test/java/com/example/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/CalculatorTest.java
@@ -10,25 +10,25 @@ public class CalculatorTest {
 
     @Test
     public void testRoot() {
-        double result = calculator.root(9);
-        assertEquals(3, result, 0.001);
+        double result = calculator.root(9.0);
+        assertEquals(3.0, result, 0.001);
     }
 
     @Test
     public void testFactorial() {
-        double result = calculator.factorial(5);
+        int result = calculator.factorial(5.0);
         assertEquals(120, result, 0.001);
     }
 
     @Test
     public void testLog() {
-        double result = calculator.log(5);
+        double result = calculator.log(5.0);
         assertEquals(1.61, result, 0.001);
     }
 
     @Test
     public void testPower() {
-        double result = calculator.power(5, 2);
+        int result = calculator.power(5.0, 2.0);
         assertEquals(25, result, 0.001);
     }
 }
